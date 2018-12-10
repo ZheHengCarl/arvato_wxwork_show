124b6e76026c5a2242a5038c331c44d315a2996b
atlantiskbz <17602108830@163.com>
2018-12-10 11:35:07 +0800

    分销页面

---
+++ pages/sale/index_bc/index.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'https://images2015.cnblogs.com/blog/1083235/201705/1083235-20170529224430274-470334084.png',
+      'https://images2015.cnblogs.com/blog/1083235/201705/1083235-20170529224451586-1329647239.png'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    margin: 40,
+    circular: true
+  }
+})
